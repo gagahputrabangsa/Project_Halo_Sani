596918b3b20950643855fb405c6d9663892548a1
gagahputrabangsa <gagaputra626@gmail.com>
2024-09-23 13:32:05 +0700

    chatbox

---
+++ sscript.js
@@ -0,0 +1,4 @@
+
+    <script id="cid0020000388059523184" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 225px;height: 286px;">
+    {"handle":"halosanibox","arch":"js","styles":{"a":"000066","b":100,"c":"FFFFFF","d":"FFFFFF","k":"000066","l":"000066","m":"000066","n":"FFFFFF","p":"10","q":"000066","r":100,"fwtickm":1}}
+    </script>
